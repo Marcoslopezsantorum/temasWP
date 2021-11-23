ce4e3fef912fb65be01eccd2d34594db53f8a21d
Mlopezsantorum <mlopezsantorum@danielcastelao.org>
2021-11-23 10:02:19 +0100

    404 correcta

--- html/wp-content/themes/newfancy/404.php
+++ html/wp-content/themes/newfancy/404.php
@@ -1,16 +1,30 @@
 <?php
+get_header()
 ?>
 
-<link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
+<div id = "Hola">
 <h1>Perro ni escribir sabes (-_-)</h1>
+</div>
+
+<div id = "pasa">
+
+
     <img src="<?php echo get_stylesheet_directory_uri();?>/imagenes/mama.jpg">
+</div>
+
+<div id = "pepe">
+
+
+
+</div>
+
 
 <div id = "contenedor">
-    <div id = "cabezera" class="rojo">
-        <h1>Hola</h1>
+    <div id = "cabecera" class="rojo">
+        <h1>Que tal la Vida bro?</h1>
     </div>
     <div id = "cuerpo">
-        <h1>Mundo</h1>
+        <h1></h1>
 
     </div>
 
@@ -20,6 +34,9 @@
 <?php
 get_footer();
 ?>
+<div id = "pepe">
+
 <h1>
 <a href = https://s.mxmcdn.net/images-storage/albums5/4/7/1/5/1/7/49715174_350_350.jpg>Solucion para arreglarlo</a>
 </h1>
+</div>
--- html/wp-content/themes/newfancy/header.php
+++ html/wp-content/themes/newfancy/header.php
@@ -1,8 +1,8 @@
 
 <div id = "cabeceras">
 
-     <h1>Hola Funcy Pro</h1>
-
+     <h1>Que tal Perrooooo</h1>
+    <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
 </div>
 
 
