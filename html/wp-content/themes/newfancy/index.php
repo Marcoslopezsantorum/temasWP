1238b098745a7f48dd9a6bb18dc1340511217e4f
Mlopezsantorum <mlopezsantorum@danielcastelao.org>
2021-11-30 10:58:26 +0100

    Añado para ver los posts

--- html/wp-content/themes/newfancy/index.php
+++ html/wp-content/themes/newfancy/index.php
@@ -1,8 +1,21 @@
-
 <?php define( 'WP_USE_THEMES', false ); get_header(); ?>
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-    <h2><a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
-    <small><?php the_time('F jS, Y'); ?> by <?php the_author_posts_link(); ?></small>
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post();  ?>
+    <div id="hola">
+        <div id="pedro">
+            <?php the_post_thumbnail(); ?>
+        </div>
+        <div>
+            <h2><a href="<?php the_permalink(); ?>" rel="bookmark"
+                   title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+            <small><?php the_time('F jS, Y'); ?> by <?php the_author_posts_link(); ?></small>
+            <div class="entry">
+                <?php the_excerpt(); ?>
+            </div>
+        </div>
+    </div>
+
+
+
 
 <?php endwhile; else : ?>
     <p><?php esc_html_e( 'Sorry, no posts matched your criteria.' ); ?></p>
