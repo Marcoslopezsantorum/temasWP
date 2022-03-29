234e4de2863a2f0aaf99dc2c60058f1910d46f95
Marcoslopezsantorum <72596886+Marcoslopezsantorum@users.noreply.github.com>
2022-03-29 10:54:39 +0200

    Delete html/wp-content/themes/newfancy directory

--- html/wp-content/themes/newfancy/footer.php
+++
@@ -1,1 +0,0 @@
-<h1>Tranqui que te ayudo</h1>
--- html/wp-content/themes/newfancy/header.php
+++
@@ -1,9 +0,0 @@
-
-<div id = "cabeceras">
-
-     <h1>Que tal Perrooooo</h1>
-    <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
-</div>
-
-
-
